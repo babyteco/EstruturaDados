7998ca271ef88e618421613f34be66747a88555f
Matheus Gon Zortéa <164538853+babyteco@users.noreply.github.com>
2025-07-13 18:49:44 +0000

    busca e ordenacao prontos

--- buca-ordenacao/main.c
+++ buca-ordenacao/main.c
@@ -1,11 +1,14 @@
+//A diferença foi clara entre o algoritmo de ordenação bubble ou o quick
+//enquanto o bubble demorou 23 segundos para ordenar um vetor de 10000 inteiros, o quick demorou menos de 1 segundo
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define max_tam_vet 100
+#define max_tam_vet 100000
 
 void bubbleSort(int *vet, int tamanho);
 
-void quickSort(int *vet, int tamanho);
+void quickSort(int *vet, int firstIndex, int lastIndex);
 
 int main(){
     int vet[max_tam_vet];
@@ -13,14 +16,16 @@ int main(){
         vet[i] = rand();
     }
     
-    for (int i = 0; i < 100; i++){
+    for (int i = 0; i < max_tam_vet; i++){
         printf("vet[%d]: %d\n", i, vet[i]);
     }
     
 
-    bubbleSort(vet, max_tam_vet);
+    // bubbleSort(vet, max_tam_vet);
+    quickSort(vet, 0, max_tam_vet - 1);
 
-    for (int i = 0; i < 100; i++){
+
+    for (int i = 0; i < max_tam_vet; i++){
         printf("vet[%d]: %d\n", i, vet[i]);
     }
 
@@ -39,6 +44,32 @@ void bubbleSort(int *vet, int tamanho){
     }
 }
 
-void quickSort(int *vet, int tamanho){
+void quickSort(int *vet, int firstIndex, int lastIndex){
+    if (lastIndex <= firstIndex){
+        return;
+    }
+    
+    int j = firstIndex;
+    int i = j - 1;
 
+
+    int pivot = vet[lastIndex];
+    while (j < lastIndex){
+        if (vet[j] < pivot){
+            i++;
+            int temp = vet[i];
+            vet[i] = vet[j];
+            vet[j] = temp;
+        }
+        
+        j++;
+    }
+
+    i++;
+    int temp = vet[i];
+    vet[i] = vet[j];
+    vet[j] = temp;
+
+    quickSort(vet, firstIndex, i - 1);
+    quickSort(vet, i + 1, lastIndex);
 }
